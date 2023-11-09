2f18706627a5ecf25f2a24838544c5440c22a10b
meenatchi <meenatchicse44@gmail.com>
2023-11-09 15:03:05 +0530

    Different hooks

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,29 @@
 // eslint-disable-next-line react/prop-types
+
+import { useNavigate } from "react-router-dom";
+
 export default function SideBar() {
+  const navigate=useNavigate();
     return (
       <div className="side-bar">
         <div>
           <ul className="actions">
-            <li>one</li>
-            <li>two</li>
+            <li>
+              <button className="btn btn-primary"
+              onClick={()=>navigate("/state")}>
+                Use Memo
+              </button>
+            </li>
+            <li>
+              <button 
+              className="btn btn-primary"
+              onClick={()=>navigate("/callback")}>
+              CallBack
+              </button>
+            </li>
             <li>three</li>
             <li>four</li>
+            
           </ul>
         </div>
       </div>
---
+++ src/pages/StatePage.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line react/prop-types
+import WorkSpace from "../components/workSpace";
+import State from "../components/state"
+
+
+const StatePage =()=>{
+    return(
+        <div>
+            <WorkSpace>
+                <State/>
+            </WorkSpace>
+        </div>
+    )
+}
+
+export default StatePage;
