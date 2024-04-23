4c27454581d7a1202b57af97ba0fb7bdc4d4a81f
meenatchi <meenatchicse44@gmail.com>
2024-04-23 19:58:05 +0530

    db

--- server/db.js
+++ server/db.js
@@ -1,12 +1,14 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
+
+
 import { MongoClient } from "mongodb";
 const Connecting = process.env.MONGO_URL;
 const LocalConnecting="mongodb://127.0.0.1:27017/"
 
 async function dbConnect(){
     try {
-        const client= new MongoClient(LocalConnecting);
+        const client= new MongoClient(Connecting);
         await client.connect();
         console.log("database connection")
         return client
